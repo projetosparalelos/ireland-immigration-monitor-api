a2588e363b7990d879da90e12c7d6fd6d89d6a2a
gpzim98 <gpzim98@gmail.com>
2017-01-20 18:45:15 +0000

    Changin the URL to our own API URLS

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,8 @@ router.register(r'searchs', views.SearchViewSet)
 
 
 urlpatterns = [
+    url(r'^renewal/$', views.renewal, name='url_renewal'),
+    url(r'^new-gnib/$', views.new_GNIB, name='url_new_GNIB'),
+    url(r'^work-permit/$', views.work_permit, name='url_work_permit'),
     url(r'^', include(router.urls)),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import viewsets
 from core.serializers import SearchSerializer
 from core.models import Search
+import urllib2, ssl, json, time
 
 
 def home(request):
@@ -15,3 +17,31 @@ class SearchViewSet(viewsets.ModelViewSet):
     """
     queryset = Search.objects.all().order_by('-date')[:1]
     serializer_class = SearchSerializer
+
+
+urlRenewal = 'https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getApps4DTAvailability)?openpage&&cat=Study&sbcat=All&typ=Renewal&_=1479407403955';
+urlNewGNIB = 'https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Study&sbcat=All&typ=New&_=1483649646705';
+urlWorkPermit = 'https://burghquayregistrationoffice.inis.gov.ie/Website/AMSREG/AMSRegWeb.nsf/(getAppsNear)?openpage&cat=Work&sbcat=All&typ=New&_=1484748845173';
+
+
+def search(url):
+    req = urllib2.Request(url, headers={ 'X-Mashape-Key': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' })
+    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)  # Only for gangstars
+    info = urllib2.urlopen(req, context=gcontext).read()
+    info = json.loads(info)
+    return info
+
+
+def renewal(request):
+    cons = search(urlRenewal)
+    return HttpResponse(json.dumps(cons), content_type="application/json")
+
+
+def new_GNIB(request):
+    cons = search(urlNewGNIB)
+    return HttpResponse(json.dumps(cons), content_type="application/json")
+
+
+def work_permit(request):
+    cons = search(urlWorkPermit)
+    return HttpResponse(json.dumps(cons), content_type="application/json")
